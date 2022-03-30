2ec66b6fd1d1f419f1bc0c4c9f5e6f2bf1521738
hfr4 <hfr4.github@gmail.com>
2022-03-30 02:56:58 +0200

    new card

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,46 +1,19 @@
-window.addEventListener("keydown", function(e) { if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) { e.preventDefault(); } }, false); // space and arrow keys
-window.onload  = function () { window.focus(); };
-window.onclick = function () { window.focus(); };
-window.onerror = function(event) {
-	draw_text_on_loading_canvas("Exception thrown, see JavaScript console");
-	Module.setStatus = function(text) { if (text) console.error("[post-exception status] " + text); };
-};
-
 var PACKAGE_FOLDER = "../assets/games/"
-var PACKAGE_NAME   = document.currentScript.getAttribute("PACKAGE_NAME").replace(/ /g, '');;
-var PACKAGE_SIZE   = 16777216;
-var PACKAGE_FILES  = [{"filename": "/game.love", "start": 0, "end": 2764592}]
+var PACKAGE_NAME   = document.currentScript.getAttribute("PACKAGE_NAME").replace(/ /g, "");;
 
-var Module = {}
-
-Module.arguments                 = ["./game.love"]
-Module.INITIAL_MEMORY            = 16777216
-Module.totalDependencies         = 0
-Module.remainingDependencies     = 0
-Module.expectedDataFileDownloads = 1;
-Module.finishedDataFileDownloads = 0;
-Module.canvas                    = document.getElementById("canvas");
-Module.printErr                  = console.error.bind(console)
-Module.preRun                    = [run_with_filesystem];
-
-Module.setStatus = function(text) {
-	if (text) {
-		draw_text_on_loading_canvas(text)
-	} else if (Module.remainingDependencies === 0) {
-		// game is loaded, hide loading_canvas and show game
-		document.getElementById("canvas").style.visibility      = "visible";
-		document.getElementById("loading_canvas").style.display = "none";
-	}
+var Module = {
+	arguments: [PACKAGE_NAME],
+	canvas   : document.getElementById("canvas"),
+	printErr : console.error.bind(console),
+	preRun   : load_game,
 }
 
-Module.monitorRunDependencies = function(left) {
-	this.remainingDependencies = left;
-	this.totalDependencies     = Math.max(this.totalDependencies, left);
-	Module.setStatus(left ? "Preparing... (" + (this.totalDependencies-left) + "/" + this.totalDependencies + ")" : "All downloads complete.");
-}
+window.addEventListener("keydown", (e) => { if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) { e.preventDefault(); } }, false); // space and arrow keys
+window.onerror = () => draw_text_on_loading_canvas("Error");
 
-Module.setStatus("Downloading...");
+draw_text_on_loading_canvas("Initializing...");
 
+// FUNCTIONS
 
 function go_full_screen(){
 	var c = document.getElementById("canvas");
@@ -51,6 +24,7 @@ function go_full_screen(){
 
 function draw_text_on_loading_canvas(text) {
 	var ctx = document.getElementById("loading_canvas").getContext("2d");
+
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 	ctx.font      = "2em Courier New";
 	ctx.textAlign = "center"
@@ -58,125 +32,22 @@ function draw_text_on_loading_canvas(text) {
 	ctx.fillText(text, ctx.canvas.scrollWidth / 2, ctx.canvas.scrollHeight / 2);
 }
 
-function run_with_filesystem() {
+function load_game() {
+	draw_text_on_loading_canvas("Downloading...");
 	
-	function DataRequest(start, end) {
-		this.start = start;
-		this.end   = end;
-	}
-	
-	DataRequest.prototype = {
-		requests: {},
+	Module.addRunDependency("rd_" + PACKAGE_NAME);
 
-		open: function(mode, name) {
-			this.name           = name;
-			this.requests[name] = this;
-			Module['addRunDependency']('fp ' + this.name);
-		},
+	var req = new XMLHttpRequest();
+	req.open("GET", PACKAGE_FOLDER + PACKAGE_NAME, true);
+	req.responseType = "arraybuffer";
+	req.onload       = (e) => {
+		var byte_array = new Uint8Array(e.target.response);
+		
+		Module.FS_createDataFile(PACKAGE_NAME, null, byte_array, true, true, true);
+		Module.removeRunDependency("rd_" + PACKAGE_NAME);
 
-		send: function() {},
-
-		onload: function() {
-			var byteArray = this.byteArray.subarray(this.start, this.end);
-			this.finish(byteArray);
-		},
-
-		finish: function(byteArray) {
-			var that = this;
-			
-			Module['FS_createDataFile'](this.name, null, byteArray, true, true, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
-			Module['removeRunDependency']('fp ' + that.name);
-			
-			this.requests[this.name] = null;
-		}
+		document.getElementById("canvas").style.display         = "unset";
+		document.getElementById("loading_canvas").style.display = "none";
 	};
-	
-	function fetch_remote_package(package_name, package_size, callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', package_name, true);
-		xhr.responseType = 'arraybuffer';
-		xhr.onprogress = function(event) {
-			var url  = package_name;
-			var size = package_size;
-
-			if (event.total) size = event.total;
-			
-			if (event.loaded) {
-				if (!xhr.addedTotal) {
-					xhr.addedTotal = true;
-					if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
-					Module.dataFileDownloads[url] = {
-						loaded: event.loaded,
-						total: size
-					};
-				} else {
-					Module.dataFileDownloads[url].loaded = event.loaded;
-				}
-	
-				var total  = 0;
-				var loaded = 0;
-				var num    = 0;
-				for (var download in Module.dataFileDownloads) {
-					var data = Module.dataFileDownloads[download];
-	
-					total  += data.total;
-					loaded += data.loaded;
-					num    += 1;
-				}
-	
-				total = Math.ceil(total * Module.expectedDataFileDownloads/num);
-	
-				Module.setStatus('Downloading data... (' + loaded + '/' + total + ')');
-	
-			} else if (!Module.dataFileDownloads) {
-				Module.setStatus('Downloading data...');
-			}
-		};
-		xhr.onerror = function(event) {
-			throw new Error("NetworkError for: " + package_name);
-		}
-		xhr.onload = function(event) {
-			if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
-				var package_data = xhr.response;
-				callback(package_data);
-			} else {
-				throw new Error(xhr.statusText + " : " + xhr.responseURL);
-			}
-		};
-		xhr.send(null);
-	};
-	
-	function process_package_data(array_buffer) {
-		Module.finishedDataFileDownloads += 1;
-		
-		if (!array_buffer)                        throw 'Loading data file failed.'         + new Error().stack;
-		if (!array_buffer instanceof ArrayBuffer) throw 'bad input to process_package_data' + new Error().stack;
-
-		var byteArray = new Uint8Array(array_buffer);
-		
-		// copy the entire loaded file into a spot in the heap. Files will refer to slices in that. They cannot be freed though
-		// (we may be allocating before malloc is ready, during startup).
-		if (Module['SPLIT_MEMORY']) Module.printErr('warning: you should run the file packager with --no-heap-copy when SPLIT_MEMORY is used, otherwise copying into the heap may fail due to the splitting');
-
-		var ptr = Module['getMemory'](byteArray.length);
-
-		Module['HEAPU8'].set(byteArray, ptr);
-		DataRequest.prototype.byteArray = Module['HEAPU8'].subarray(ptr, ptr+byteArray.length);
-		
-		for (i = 0; i < PACKAGE_FILES.length; ++i) DataRequest.prototype.requests[PACKAGE_FILES[i].filename].onload();
-		
-		Module.removeRunDependency('datafile_game.data');
-	};
-
-	for (i = 0; i < PACKAGE_FILES.length; ++i) new DataRequest(PACKAGE_FILES[i].start, PACKAGE_FILES[i].end).open('GET', PACKAGE_FILES[i].filename);
-
-	Module['addRunDependency']('datafile_game.data');
-	
-	if (!Module.preloadResults) Module.preloadResults = {};
-
-	console.info('loading ' + PACKAGE_NAME + ' from remote');
-
-	fetch_remote_package(PACKAGE_FOLDER + PACKAGE_NAME, PACKAGE_SIZE, function(package_data) { process_package_data(package_data); });
-
-	Module.setStatus('Downloading...');
+	req.send();
 }
