c427e8e500250abcc5aa141ad750d4315911b3a4
hfr4 <30574115+hfr4@users.noreply.github.com>
2022-08-03 17:27:16 +0200

    gallery video

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,10 +1,26 @@
-function open_gallery(img_src) {
-	document.getElementById("gallery_img").src = img_src;
-	document.getElementById("gallery_modal").classList.add("visible")
+function open_gallery_img(img_src) {
+	document.getElementById("gallery_img_img").src = img_src;
+	document.getElementById("gallery_img_modal").classList.add("visible")
 	document.body.style.overflow = "hidden";
 }
 
-function close_gallery() {
-	document.getElementById("gallery_modal").classList.remove("visible")
+function close_gallery_img() {
+	document.getElementById("gallery_img_modal").classList.remove("visible")
+	document.body.style = "";
+}
+
+function open_gallery_vid(vid_src) {
+    document.getElementById("gallery_vid_source").src = vid_src;
+	document.getElementById("gallery_vid_video").poster = vid_src;
+    document.getElementById("gallery_vid_video").load();
+    document.getElementById("gallery_vid_video").play();
+	document.getElementById("gallery_vid_modal").classList.add("visible")
+	document.body.style.overflow = "hidden";
+}
+
+function close_gallery_vid() {
+    document.getElementById("gallery_vid_video").pause();
+    document.getElementById("gallery_vid_video").currentTime = 0;
+	document.getElementById("gallery_vid_modal").classList.remove("visible")
 	document.body.style = "";
 }
